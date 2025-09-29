b5d3b57e1e075341a2c13ad6f649876070a4c204
Omari-mobutu <mobutucnc@hotmail.com>
2025-09-29 21:03:37 +0300

    added an alias to the radacct model for the class object

--- app/models/radacct.rb
+++ app/models/radacct.rb
@@ -2,6 +2,9 @@
 class Radacct < ApplicationRecord
   self.table_name = "radacct"
   self.primary_key = "radacctid"
+
+  # Alias the problematic 'class' column to a safe name like 'session_class'
+  alias_attribute :session_class, :class
   # Prevent accidental writes
   def readonly?
     true
